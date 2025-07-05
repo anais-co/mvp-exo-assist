8e4f6945a960dc5151d19d16514d6b0c7a722648
Anaïs <anais.zerouklane@etu.minesparis.psl.eu>
2025-07-05 01:30:50 +0100

    Update settings.py

--- boilerplatev5/boilerplatev5/settings.py
+++ boilerplatev5/boilerplatev5/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+EXERCICE_IMGS_ROOT = os.path.join(BASE_DIR, "exercice_imgs")
+EXERCICE_IMGS_URL = "/exercice_imgs/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
