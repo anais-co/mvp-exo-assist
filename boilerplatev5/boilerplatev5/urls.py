071d2e15440fdb2f9267a5bd996eaa3828239dd6
Anaïs <anais.zerouklane@etu.minesparis.psl.eu>
2025-07-05 01:31:10 +0100

    Update urls.py

--- boilerplatev5/boilerplatev5/urls.py
+++ boilerplatev5/boilerplatev5/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for boilerplatev5 project.
+URL configuration for archimaidev2 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('exercises.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.EXERCICE_IMGS_URL, document_root=settings.EXERCICE_IMGS_ROOT)
